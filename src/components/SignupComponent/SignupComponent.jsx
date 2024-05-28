4c994c678585bfcff670ba420452b5e50c12da62
Giridharan <slbg.giridharan@gmail.com>
2024-05-28 16:51:58 +0530

    commit

--- src/components/SignupComponent/SignupComponent.jsx
+++ src/components/SignupComponent/SignupComponent.jsx
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import logo from "../../assets/todolist-logo.png";
 import { register } from "../../service/authservice";
 import { useNavigate } from "react-router-dom";
+
 function SignupComponent() {
   const [firstName, setFirstName] = React.useState("");
   const [lastName, setLastName] = React.useState("");
@@ -14,40 +15,40 @@ function SignupComponent() {
 
   const navigator = useNavigate();
 
-  function validation(e) {
-    e.preventDefault();
+  function validation() {
     if (!firstName) {
       setErrorMessage("Enter First Name");
-      return;
+      return false;
     } else if (!lastName) {
       setErrorMessage("Enter Last Name");
-      return;
+      return false;
     } else if (!email) {
       setErrorMessage("Enter email");
-      return;
+      return false;
     } else if (!email.includes("@") || !email.includes(".")) {
       setErrorMessage("Enter a valid email");
-      return;
+      return false;
     } else if (!password) {
       setErrorMessage("Enter password");
-      return;
+      return false;
     } else if (!confirmPassword) {
       setErrorMessage("Confirm password");
-      return;
+      return false;
     } else if (password.length < 8) {
-      setErrorMessage("Password must have length of atleast 8 characters");
-      return;
-    } else if (password !== password) {
+      setErrorMessage("Password must have length of at least 8 characters");
+      return false;
+    } else if (password !== confirmPassword) {
       setErrorMessage("Passwords should match");
-      return;
+      return false;
     } else {
       setErrorMessage("");
+      return true;
     }
   }
 
   function handleSubmit(e) {
     e.preventDefault();
-    if (validation) {
+    if (validation()) {
       const user = {
         id: null,
         firstName: firstName,
@@ -67,58 +68,56 @@ function SignupComponent() {
   }
 
   return (
-    <>
-      <div className="signupContainer">
-        <div className="signupSection">
-          <div className="logo">
-            <img src={logo} alt="TodoList Logo" />
-            To-Do-It
-          </div>
-          <form className="inputContainer" onSubmit={handleSubmit}>
-            <input
-              type="text"
-              placeholder="First Name"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="Last Name"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <input
-              type="password"
-              placeholder="Confirm Password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-            {errorMessage && <p className="errorMessage">{errorMessage}</p>}
-            <div className="signupButtonContainer">
-              <button type="submit" className="signupButton">
-                Sign up
-              </button>
-            </div>
-          </form>
-          <p>
-            Already have an account?
-            <a href="/login"> Log in</a>
-          </p>
+    <div className="signupContainer">
+      <div className="signupSection">
+        <div className="logo">
+          <img src={logo} alt="TodoList Logo" />
+          To-Do-It
         </div>
+        <form className="inputContainer" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="First Name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          {errorMessage && <p className="errorMessage">{errorMessage}</p>}
+          <div className="signupButtonContainer">
+            <button type="submit" className="signupButton">
+              Sign up
+            </button>
+          </div>
+        </form>
+        <p>
+          Already have an account?
+          <a href="/login"> Log in</a>
+        </p>
       </div>
-    </>
+    </div>
   );
 }
 
